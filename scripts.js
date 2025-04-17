1d8fcb4d07e47fbef20318004455927d35b70613
quiquelondon <enrique@engomez.com>
2025-04-17 08:48:49 +0200

    initial commit

---
+++ scripts.js
@@ -0,0 +1,59 @@
+function toggleSidebar() {
+const sidebar = document.querySelector('.sidebar');
+sidebar.classList.toggle('active');
+}
+
+document.addEventListener("click", (event) => {
+const sidebar = document.querySelector('.sidebar');
+const hamburger = document.querySelector('.hamburger');
+
+if (sidebar.classList.contains('active') && 
+!sidebar.contains(event.target) && 
+!hamburger.contains(event.target)) {
+sidebar.classList.remove('active');
+}
+});;
+
+fetch('header-mobile.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('header-mobile').innerHTML = html;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
+
+fetch('navbar.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('navbar').innerHTML = html;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
+
+
+    document.addEventListener('click', (event) => {
+        if (event.target.classList.contains('dropdown-btn')) {
+            const button = event.target;
+            const subnav = button.nextElementSibling;
+            const isOpen = subnav.classList.contains('show');
+    
+            document.querySelectorAll('.subnav').forEach((menu) => {
+                if (menu !== subnav) {
+                    menu.classList.remove('show');
+                }
+            });
+    
+            if (isOpen) {
+                subnav.classList.remove('show');
+            } else {
+                subnav.classList.add('show');
+            }
+        }
+    });
+
+
+    // Select all navigation links
+const navLinks = document.querySelectorAll('.navbar a');
+
+// Get the current URL path
+const currentPath = window.location.pathname;
+
+
